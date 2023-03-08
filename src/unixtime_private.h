dc31a2393483bf5b9c7fcde8ec19efcda996625c
maddsua <102395937+maddsua@users.noreply.github.com>
2023-03-08 22:12:24 +0200

    v1.1
    Add LICENSE
    Add CHANGELOG
    Update LICENSE

---
+++ src/unixtime_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef UNIXTIME_PRIVATE_H
+#define UNIXTIME_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.1.0.311"
+#define VER_MAJOR	1
+#define VER_MINOR	1
+#define VER_RELEASE	0
+#define VER_BUILD	311
+#define COMPANY_NAME	"maddsua"
+#define FILE_VERSION	"1.1.0.311"
+#define FILE_DESCRIPTION	"Unix to Readable Time Converter"
+#define INTERNAL_NAME	"unixtime"
+#define LEGAL_COPYRIGHT	"https://gitlab.com/maddsua/"
+#define LEGAL_TRADEMARKS	"2022, maddsua"
+#define ORIGINAL_FILENAME	"utime"
+#define PRODUCT_NAME	"Unix Time Converter"
+#define PRODUCT_VERSION	"1.1.0.311"
+
+#endif /*UNIXTIME_PRIVATE_H*/
